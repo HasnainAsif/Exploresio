233c37d122436d070880a930671ba1d7e493861d
Hasnain Asif <hasnainasif52@gmail.com>
2020-08-13 16:08:12 +0500

    Add is ready to launch

--- config/db.js
+++ config/db.js
@@ -1,18 +1,24 @@
-const mongoose = require('mongoose')
-const config = require('config')
-const db = config.get('mongoURI')
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db , {useUnifiedTopology : true, useNewUrlParser : true,useCreateIndex:true})
-        console.log('MongoDB Connected...')
-    } catch (error) {
-        console.log(error.message)
-        //Exit Process with Failure
-        process.exit(1)
-    }
-    //Mongoose Deprication Warning
-    mongoose.set('useFindAndModify', false);
-}
+  // await mongoose.connect(process.env.MONGODB_URI || db , {useUnifiedTopology : true, useNewUrlParser : true,useCreateIndex:true})
+  //     console.log('MongoDB Connected...')
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.log(error.message);
+    //Exit Process with Failure
+    process.exit(1);
+  }
+  //Mongoose Deprication Warning
+  mongoose.set("useFindAndModify", false);
+};
 
-module.exports = connectDB
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -36,6 +36,17 @@ app.use("/admin/newsletter", require("./routes/admin/newsletter"));
 app.use("/admin/tour", require("./routes/admin/tour"));
 app.use("/admin/custom_tour", require("./routes/admin/custom_tour"));
 
+///////// For Heroku Deployment
+//Serve Statis assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
 
 // var count = 0;
